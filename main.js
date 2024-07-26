c3b52238441a351f6c0ce509a899e2d123e7d259
anasxonbahodirov <anasxonbahodirov1@gmail.com>
2024-07-26 10:44:11 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const name = prompt("Ismingizni kiriting:");
+
+const spaceId = prompt("Space ID ni kiriting:");
+
+const spacePassword = prompt("Space Password ni kiriting:");
+
+const oka = {
+    name: name,
+    id: spaceId,
+    password: spacePassword
+};
+
+console.log(oka);
